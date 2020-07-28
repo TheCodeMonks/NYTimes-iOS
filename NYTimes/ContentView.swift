5a0785a27958315e2ddf46751a07d87a69dee2aa
W4533M <waseem07799@gmail.com>
2020-07-28 14:57:51 +0530

    Initial Commit

---
+++ NYTimes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NYTimes
+//
+//  Created by Waseem Akram on 28/07/20.
+//  Copyright © 2020 Waseem Akram. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
